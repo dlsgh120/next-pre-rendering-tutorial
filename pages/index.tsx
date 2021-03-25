32c8ee2b23bca0aa6cf0804e721add732d0da207
dlsgh120 <dlsgh120@gmail.com>
2021-03-25 19:07:01 +0900

    change index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,6 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
 
-const PostLink = (props:{title:string}) =>{
-  return(
-    <li>
-        <Link href={`/post?title=${props.title}`}>
-            <a>{props.title}</a>
-        </Link>
-    </li>
-  );
-}
-
 const BlogLink = (props:{id:string}) =>{
   return(
     <li>
@@ -24,12 +14,6 @@ const BlogLink = (props:{id:string}) =>{
 const Home = () =>{
   return (
         <Layout>
-            <h1>Post - query parameter</h1>
-            <ul>
-                <PostLink title="hello,Next.js" />
-                <PostLink title="Next.js query parameter" />
-                <PostLink title="Next.js is Awesome" />
-            </ul>
             <h1>Blog - pathname parameter</h1>
             <ul>
               <BlogLink id="1" />
